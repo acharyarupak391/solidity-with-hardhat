70a1d9bc45ec19d4736b6140d625fd780fe597b5
acharyarupak391 <acharyarupak391@gmail.com>
2022-03-16 16:17:07 +0545

    added deployment to live network

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,18 @@ require("@nomiclabs/hardhat-waffle");
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
+require("dotenv").config();
+
+const kovanUrl = process.env["KOVAN_DEPLOY_URL"];
+const kovanAccount = process.env["ACCOUNT_PRIVATE_KEY"];
+
 module.exports = {
   solidity: "0.7.3",
+  networks: {
+    kovan: {
+      url: kovanUrl,
+      accounts: [kovanAccount]
+    }
+  }
 };
