c412f56faa31abda8c03ebf5ef09177ee65e6ddb
acharyarupak391 <acharyarupak391@gmail.com>
2022-09-12 16:54:58 +0545

    config & scripts updated

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,15 +6,33 @@ require("@nomiclabs/hardhat-waffle");
 
 require("dotenv").config();
 
+const account = process.env["ACCOUNT_PRIVATE_KEY"];
+
 const kovanUrl = process.env["KOVAN_DEPLOY_URL"];
-const kovanAccount = process.env["ACCOUNT_PRIVATE_KEY"];
+const mainnetUrl = process.env["MAINNET_DEPLOY_URL"];
+const goerliUrl = process.env["GOERLI_DEPLOY_URL"];
+
+const networks = {};
+
+if (kovanUrl)
+  networks.kovan = {
+    url: kovanUrl,
+    accounts: [account],
+  };
+
+if (mainnetUrl)
+  networks.mainnet = {
+    url: mainnetUrl,
+    accounts: [account],
+  };
+
+if (goerliUrl)
+  networks.goerli = {
+    url: goerliUrl,
+    accounts: [account],
+  };
 
 module.exports = {
   solidity: "0.7.3",
-  networks: {
-    kovan: {
-      url: kovanUrl,
-      accounts: [kovanAccount]
-    }
-  }
+  networks,
 };
